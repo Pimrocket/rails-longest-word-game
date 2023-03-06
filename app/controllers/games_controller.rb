ed7cef52f2c1e72ef32b25648a8e86901853f373
Simen Woldmo <simen.w.93@hotmail.com>
2023-03-06 21:29:55 +0100

    Rails-longest-word-game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,30 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  VOWELS = ["A", "E", "I", "O", "U", "Y"]
+  # NO_VOWELS = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z']
+  def new
+    # @alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    @no_vowels = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z']
+    @letters = @no_vowels.sample(5) + VOWELS.sample(5)
+    @letters.shuffle!
+  end
+
+  def score
+    @word = (params[:word] || '').upcase
+    @letters = params[:letters].split
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+    # raise
+  end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
